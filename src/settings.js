497cc53c5dee51888a6645ac4357d33de67c4574
Glorentino <86209684+Glorentino@users.noreply.github.com>
2022-08-24 19:31:14 -0400

    Removed Api key

--- src/settings.js
+++ src/settings.js
@@ -1,7 +1,5 @@
 import { createClient, createMicrophoneAndCameraTracks } from "agora-rtc-react";
 
-const appID = "a7fb2830a3a8478cb750e1bdb7b2d970";
-const token = "006a7fb2830a3a8478cb750e1bdb7b2d970IAA45cGal8RDH/damIhPx8pTx4FIZ/y82TN2H5dICpFJ7WTNKL8AAAAAEADYBo2ZQYr9YgEAAQBAiv1i";
 
 
 export const config = {mode: "rtc", codec: "vp8", appID: appID, token: token};
